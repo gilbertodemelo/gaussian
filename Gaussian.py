97a849c801a1c25a4918511982c08e4e3a05cc0b
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2022-04-06 10:18:51 -0300

    gaussian class complete

--- Gaussian.py
+++ Gaussian.py
@@ -25,11 +25,11 @@ class Gaussian:
         Returns:
             float: mean of the data set
         """
-        sum= 0
+        sum_of_data = 0
         for item in self.data:
-            sum += item
+            sum_of_data += item
 
-        return sum / len(self.data)
+        return 1.0 * sum_of_data / len(self.data)
 
     def calculate_stdev(self, sample=True) -> float:
         """Method to calculate the standard deviation of the data set.
@@ -50,7 +50,9 @@ class Gaussian:
             variance = sum_of_squares / (len(self.data) - 1)
         else:
             variance = sum_of_squares / len(self.data)
-        return math.sqrt(variance)
+
+        self.stdev = math.sqrt(variance)
+        return self.stdev
 
     def read_data_file(self, file_name, sample=True):
         """Method to read in data from a txt file. The txt file should have
@@ -88,3 +90,63 @@ class Gaussian:
         Returns:
             None
         """
+        plt.hist(self.data)
+        plt.title('Histogram of Data')
+        plt.xlabel('data')
+        plt.ylabel('count')
+
+    def pdf(self, x) -> float:
+        """Probability density function calculator for the gaussian distribution.
+
+        Args:
+            x (float): point for calculating the probability density function
+
+        Returns:
+            float: probability density function output
+        """
+        return (1.0 / (self.stdev * math.sqrt(2 * math.pi))) * math.exp(-0.5*((x - self.mean) / self.stdev) ** 2)
+
+    def plot_histogram_pdf(self, n_spaces = 50):
+        """Function to plot the normalized histogram of the data and a plot of the
+        probability density function along the same range
+
+        Args:
+            n_spaces (int): number of data points
+
+        Returns:
+            list: x values for the pdf plot
+            list: y values for the pdf plot
+
+        """
+
+        mu = self.mean
+        sigma = self.stdev
+
+        min_range = min(self.data)
+        max_range = max(self.data)
+
+        # calculates the interval between x values
+        interval = 1.0 * (max_range - min_range) / n_spaces
+
+        x = []
+        y = []
+
+        # calculate the x values to visualize
+        for i in range(n_spaces):
+            tmp = min_range + interval*i
+            x.append(tmp)
+            y.append(self.pdf(tmp))
+
+        # make the plots
+        fig, axes = plt.subplots(2,sharex=True)
+        fig.subplots_adjust(hspace=.5)
+        axes[0].hist(self.data, density=True)
+        axes[0].set_title('Normed Histogram of Data')
+        axes[0].set_ylabel('Density')
+
+        axes[1].plot(x, y)
+        axes[1].set_title('Normal Distribution for \n Sample Mean and Sample Standard Deviation')
+        axes[0].set_ylabel('Density')
+        plt.show()
+
+        return x, y
