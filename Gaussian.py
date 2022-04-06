641db8398f15cdaed5c89ddcb3abe98e0cb5e0ab
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2022-04-06 09:48:36 -0300

    read file method

--- Gaussian.py
+++ Gaussian.py
@@ -51,3 +51,40 @@ class Gaussian:
         else:
             variance = sum_of_squares / len(self.data)
         return math.sqrt(variance)
+
+    def read_data_file(self, file_name, sample=True):
+        """Method to read in data from a txt file. The txt file should have
+        one number (float) per line. The numbers are stored in the data attribute.
+        After reading in the file, the mean and standard deviation are calculated
+
+        Args:
+            file_name (string): name of a file to read from
+
+        Returns:
+            None
+        """
+
+        # This code opens a data file and appends the data to a list called data_list
+        with open(file_name) as file:
+            data_list = []
+            line = file.readline()
+            while line:
+                data_list.append(int(line))
+                line = file.readline()
+        file.close()
+
+        # Copy data_list to self.data attribute
+        self.data = data_list.copy()
+        self.mean = self.calculate_mean()
+        self.stdev = self.calculate_stdev(sample)
+
+    def plot_histogram(self):
+        """Method to output a histogram of the instance variable data using
+        matplotlib pyplot library.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
